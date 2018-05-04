efd35cba1a38ddfc8059ccfe5d98d8e6fa624f1b
root <zvxmhwfn>
2018-05-04 11:43:26 +0200

    Implemented main loop

--- sniffer.py
+++ sniffer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import socket
+
 
 def main():
-    return
+    s = socket.socket(
+            socket.AF_PACKET,
+            socket.SOCK_RAW,
+            socket.htons(3)
+    )
+    s.bind(('enp0s3', 3))
+
+    while True:
+        message = s.recv(1024)
+        print(repr(message))
 
 
 if __name__ == '__main__':
